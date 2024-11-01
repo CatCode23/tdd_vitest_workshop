79469493faa4a201ffd57a79d1fc7d33c9675324
CatCode23 <andrewcatalano23@gmail.com>
2024-11-01 15:44:44 -0400

    first test

--- tdd_react/src/app.test.jsx
+++ tdd_react/src/app.test.jsx
@@ -8,25 +8,27 @@ import App from './App'
 //   })
 // })
 const BASE_URL = "http://localhost:3001/movies";
+// describe("should gimme back some movies", () => {
+//     it("should show the first movie by ID", async() => {
+//         const response = await fetch(`${BASE_URL}`)
+//         const movie = await response.json();
 
-describe("should gimme back some movies", () => {
-    it("should show the first movie by ID", async() => {
-        const response = await fetch(`${BASE_URL}/1`)
-        const movie = await response.json();
-
-        expect(movie).toEqual({movieId: 1})
+//         expect(movie).toEqual({movieId: 1})
 
 
-    })
+//     })
 
-})
+// })
 
 
 describe('App', () => {
-  it('renders the App component', () => {
+  it('renders the App component', async () => {
     render(<App />)
     
-    screen.debug(); // prints out the jsx in the App component unto the command line
-  })
+    screen.debug(); 
+    
+
+    const starWarsImage = await screen.findByRole('img', { name: /Star Wars: Episode IV - A New Hope/i });
+    expect(starWarsImage).toHaveAttribute('src', "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg");  })
 })
 
